b12abd88312b507f10b300ae0910b5d3dacb70bc
falandi <91754274+falandi@users.noreply.github.com>
2023-04-03 12:02:46 -0700

    Update microservices.groovy

--- microservices.groovy
+++ microservices.groovy
@@ -15,7 +15,83 @@ steps {
 }
 
 
+stage('Unit Test') { 
+steps { 
+    
+    script {
+        def test_reports_exist = fileExists 'test-reports'
+        if (test_reports_exist) {
+            sh 'rm test-reports/*.xml || true'
+        }
+        def api_test_reports_exist = fileExists 'api-test-reports'
+        if (api_test_reports_exist) {
+            sh 'rm api-test-reports/*.xml || true'
+        }
 
+        def test_reports_car_exist = fileExists 'test-reports-car'
+        if (test_reports_car_exist) {
+            sh 'rm test-reports-car/*.xml || true'
+        }
+
+         def test_reports_instructor_exist = fileExists 'test-reports-instructor'
+        if (test_reports_instructor_exist) {
+            sh 'rm test-reports-instructor/*.xml || true'
+        }
+
+    }
+    
+    script {
+        def files = findFiles(glob: 'test*.py')
+        for (file in files){
+            sh "coverage run --omit */site-packages/*,*/dist-packages/* ${file}"
+            sh 'coverage report' 
+        }
+
+    }
+    
+    script {
+        def test_reports_exist = fileExists 'test-reports'
+            if (test_reports_exist) {
+                junit 'test-reports/*.xml'
+            }
+        def api_test_reports_exist = fileExists 'api-test-reports'
+            if (api_test_reports_exist) {
+                junit 'api-test-reports/*.xml'
+            }
+
+        def test_reports_car_exist = fileExists 'test-reports-car'
+            if (test_reports_car_exist) {
+                junit 'test-reports-car/*.xml'
+            }
+
+        def test_reports_instructor_exist = fileExists 'test-reports-instructor'
+            if (test_reports_instructor_exist) {
+                junit 'test-reports-instructor/*.xml'
+            }
+
+    }
+  
+   
+} 
+
+}
+
+stage('Zip Artifacts') {
+steps{
+    script {
+        sh 'zip -r app.zip *.py'
+    }
+    
+    script{
+        archiveArtifacts artifacts: 'app.zip',
+                   allowEmptyArchive: true,
+                   fingerprint: true,
+                   onlyIfSuccessful: true
+    }
+
+    
+    
+}
 }
 
 }
